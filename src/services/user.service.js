78b8439f56ceb7ee7ed296508a3160e71c0ec8c9
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2023-08-08 14:46:21 -0300

    fix(req06): corrige erro de ESLint
    - Corrige o erro de linter no arquivo `user.service.js`.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -28,11 +28,12 @@ const getAllUsers = async () => {
 const getUserById = async (id) => {
   const user = await User.findByPk(id, { attributes: { exclude: ['password'] } });
 
-  if (!user) return {
-    status: 404,
-    data: { message: 'User does not exist' },
-  };
-
+  if (!user) {
+    return {
+      status: 404,
+      data: { message: 'User does not exist' },
+    };
+  }
   return {
     status: 200,
     data: user,
