ca8f4b4c94bdabe019218a2aafb312e399522a31
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2023-08-08 14:52:57 -0300

    feat(req07): Implementa o modelo `Category`
    - Adiciona o arquivo Category.js no diretório src/models.
    - Cria o modelo Category com as propriedades corretas, conforme especificado no diagrama de Entidade-Relacionamento.
    - O modelo Category inclui as propriedades id e name.
    - O modelo Category é desenvolvido no formato funcional, não como uma classe.

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false, // Não permite nulo
+      autoIcrement: true, // Define que é autoincremento
+      primaryKey: true, // Define a chave primária
+      type: DataTypes.INTEGER, // Define o tipo de dado
+    },
+    name: {
+      allowNull: false, // Não permite nulo
+      type: DataTypes.STRING, // Define o tipo de dado
+    },
+  }, {
+    timestamps: false, // Não cria colunas createdAt e updatedAt
+    tableName: 'categories', // Nome da tabela
+    underscored: true, // CamelCase para snake_case
+  });
+
+  return Category;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -1,27 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: {
-      allowNull: false, // Não permite nulo
-      autoIncrement: true, // Auto incremento
-      primaryKey: true, // Chave primária
-      type: DataTypes.INTEGER, // Tipo inteiro
+      allowNull: false, 
+      autoIncrement: true, 
+      primaryKey: true, 
+      type: DataTypes.INTEGER, 
     }, 
     displayName: {
-      allowNull: false, // Não permite nulo
-      type: DataTypes.STRING, // Tipo string
+      allowNull: false, 
+      type: DataTypes.STRING,
     },
     email: {
-      allowNull: false, // Não permite nulo
-      type: DataTypes.STRING, // Tipo string
-      unique: true, // Único
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
     },
     password: {
-      allowNull: false, // Não permite nulo
-      type: DataTypes.STRING, // Tipo string
+      allowNull: false,
+      type: DataTypes.STRING,
     },
     image: {
-      allowNull: false, // Não permite nulo
-      type: DataTypes.STRING, // Tipo string
+      allowNull: false,
+      type: DataTypes.STRING,
     },
   }, {
     timestamps: false,
