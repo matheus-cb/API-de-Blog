d5835ee942d5be2c1cbdb79d5e6752a0023ea2ea
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2023-08-08 23:02:10 -0300

    feat(req11): Implementa o modelo `PostCategory` e suas associações
    - Adiciona o arquivo PostCategory.js no diretório src/models.
    - Cria o modelo PostCategory com as propriedades corretas, conforme especificado no diagrama de Entidade-Relacionamento.
    - Estabelece a associação correta (N:N) entre o modelo BlogPost e o modelo Category.
    - Adiciona a associação belongsToMany no modelo PostCategory para as entidades Category e BlogPost.

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    categoryId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+}
